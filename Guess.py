89e79a17b487f4fe212a92f872e5f8f3340035c1
Noam <46218256+jackfish823@users.noreply.github.com>
2020-01-06 01:41:25 +0200

    Third assignment - tar 3.5.2
    - Added enter word input + prints
      blanks for the word length

--- Guess.py
+++ Guess.py
@@ -1,4 +1,9 @@
 
-guess_input = input("Guess a letter: ")
+word_input = input("Enter word: ")
+word_len = int(len(word_input))
+
+print("_ " * word_len)
+
+guess_input = input("\nGuess a letter: ")
 
 print(guess_input.lower())
