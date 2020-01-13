afd610fc74d0f83fbb1056f62e9f6af1f0828891
Noam <46218256+jackfish823@users.noreply.github.com>
2020-01-13 23:04:10 +0200

    Update Guess.py

--- Guess.py
+++ Guess.py
@@ -1,25 +1,22 @@
 import re  #lib to search in a string
 
+
+def is_valid_input(letter_guessed):
+    # checks if the function the input letter_guessed is good
+    spread_guess = re.findall('[A-Za-z]', letter_guessed)  #list of the guess_input of only english
+
+    if len(letter_guessed) == len(spread_guess):
+        if len(letter_guessed) == 1:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
 word_input = input("Enter word: ")
 word_len = int(len(word_input))
 
 print("_ " * word_len)  #print the _ _ _ _
 
 guess_input = input("\nGuess a letter: ")  #to guess the letter
-
-
-#          -validation check-            #
-spread_guess = re.findall('[A-Za-z]', guess_input)  #list of the guess_input of only english
-
-if len(guess_input) == len(spread_guess):
-    if len(guess_input) == 1:
-        print(guess_input.lower())
-    else:
-        print("E1")
-
-else:
-    if len(guess_input) == 1:
-        print("E2")
-    else:
-        print("E3")
-#---------------------------------------#
